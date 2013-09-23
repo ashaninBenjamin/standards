f139476e385722378e055f3ab75eb94f88706200
Benjamin <ashanin.benjamin@gmail.com>
2013-09-24 02:28:14 +0400

    fix standard types

--- app/types/standard_new_type.rb
+++ app/types/standard_new_type.rb
@@ -1,7 +1,7 @@
 class StandardNewType < Standard
   include BaseType
 
-  permit :name, :number, :content, :state, :access_state, :access_state_event, :parent_id
+  permit :name, :number, :content, :state, :state_event, :access_state, :access_state_event, :parent_id
 
   def available_numbers(user)
     user.standards.roots.first.node_numbers
--- app/types/standard_type.rb
+++ app/types/standard_type.rb
@@ -1,6 +1,6 @@
 class StandardType < Standard
   include BaseType
 
-  permit :name, :number, :content, :state, :access_state, :access_state_event, :parent_id
+  permit :name, :number, :content, :state, :state_event, :access_state, :access_state_event, :parent_id
 
 end
