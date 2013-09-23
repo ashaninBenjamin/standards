9126e9c4a9b1aaaa07dd63fc6164174b195b8cfd
Benjamin <ashanin.benjamin@gmail.com>
2013-09-24 01:52:18 +0400

    add locales, improve views

--- app/helpers/web/standards_helper.rb
+++ app/helpers/web/standards_helper.rb
@@ -1,9 +1,9 @@
 module Web::StandardsHelper
   def standard_access_states
-    Standard.state_machine(:access_state).states.map {|s| [s.human_name, s.name]}
+    Standard.state_machine(:access_state).events.map {|s| [s.human_name, s.name]}
   end
 
   def standard_states
-    Standard.state_machine(:state).states.map {|s| [s.human_name, s.name]}
+    Standard.state_machine(:state).events.map {|s| [s.human_name, s.name]}
   end
 end
