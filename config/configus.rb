0b75e6ddffbf3d7d2a42bb13fce8cdc26c1d9f1e
Benjamin <ashanin.benjamin@gmail.com>
2013-10-19 14:00:21 +0400

    add techical support mailer

--- config/configus.rb
+++ config/configus.rb
@@ -4,6 +4,7 @@ Configus.build Rails.env do
       default_host '1standart.com'
       default_from '1st@1standart.com'
     end
+    techical_support_email '1standard.dev@gmail.com'
   end
 
   env :development, parent: :production
