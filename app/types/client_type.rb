92ff7febdbbf0ff8dc53bc29583433253c96a3b3
Benjamin <ashanin.benjamin@gmail.com>
2013-11-01 11:39:18 +0400

    add permitted fields in type for client

--- app/types/client_type.rb
+++ app/types/client_type.rb
@@ -3,6 +3,6 @@ class ClientType < Client
 
   has_secure_password
 
-  permit :email, :password, :password_confirmation, :names
+  permit :email, :password, :password_confirmation, :names, :opf, :company_name, :company_city, :phone_number
 
 end
